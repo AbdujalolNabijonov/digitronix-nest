e4ead03fce3031e40ea73098e8797bf6525f1534
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-07-10 11:49:29 +0900

    feat: Intigrate .env configModule

--- apps/digitonix-batch/src/main.ts
+++ apps/digitonix-batch/src/main.ts
@@ -3,6 +3,6 @@ import { DigitonixBatchModule } from './digitonix-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(DigitonixBatchModule);
-  await app.listen(3007);
+  await app.listen(process.env.PORT_BATCH);
 }
 bootstrap();

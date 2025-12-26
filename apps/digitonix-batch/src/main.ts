e3ad8cffdbcee15f6f2708803af810a3241832e5
AbdujalolNabijonov <mrarnoldunlimited@gmail.com>
2025-12-27 07:12:08 +0900

    fix: Batch server build script correction

--- apps/digitonix-batch/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { DigitonixBatchModule } from './digitonix-batch.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(DigitonixBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 4002);
-}
-bootstrap();
